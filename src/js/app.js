6d8663aeee37bcd417e5023f3dec4d80c6aa8e05
Patrick <paddyloldev@gmail.com>
2019-03-07 14:23:01 +0000

    Boilerplate

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+/*
+    this is where all your core javascript will go that
+    you traditionally include in the <script></script> tags
+*/
+//import our styles
+import '../css/main.scss'
+
+//import our test function from another js file
+import {test} from './models/test.js'
+
+let button = document.querySelector('.clickme');
+
+button.onclick = () =>{
+    test();
+}
---
+++ src/js/models/test.js
@@ -0,0 +1,4 @@
+function test(){
+    document.querySelector('a').style.color = 'blue';
+}
+module.exports = { test }
