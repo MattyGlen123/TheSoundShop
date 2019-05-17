3dbd3aab6e621ece9c2ea4b39df0bbad67ef771c
Matty <matty@mattys.home>
2019-05-17 20:49:11 +0100

    adding base styles

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,6 @@
-/*
-    this is where all your core javascript will go that
-    you traditionally include in the <script></script> tags
-*/
 //import our styles
 import '../css/main.scss'
 
-//import our test function from another js file
-import {test} from './models/test.js'
+//import js file
+import {test} from './model/test.js'
 
